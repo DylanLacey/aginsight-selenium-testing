ae540c85a973fb54a691e5fa4b6bf17d5423605d
Jordan King <jking@esriaustralia.com.au>
2016-08-31 16:07:20 +0930

    update

--- tests/base_test.py
+++ tests/base_test.py
@@ -25,16 +25,16 @@ from UIComponentObjects import Tour, Footer, Header, Highlights, LeftPanel, Righ
 
 
 # This decorator is required to iterate over browsers
-def on_platforms(platforms):
-    def decorator(base_class):
-        module = sys.modules[base_class.__module__].__dict__
-        for i, platform in enumerate(platforms):
-            d = dict(base_class.__dict__)
-            d['desired_capabilities'] = platform
-            name = "%s_%s" % (base_class.__name__, i + 1)
-            module[name] = new.classobj(name, (base_class,), d)
-
-    return decorator
+# def on_platforms(platforms):
+#     def decorator(base_class):
+#         module = sys.modules[base_class.__module__].__dict__
+#         for i, platform in enumerate(platforms):
+#             d = dict(base_class.__dict__)
+#             d['desired_capabilities'] = platform
+#             name = "%s_%s" % (base_class.__name__, i + 1)
+#             module[name] = new.classobj(name, (base_class,), d)
+#
+#     return decorator
 
 
 class BaseTest(unittest.TestCase):
@@ -48,20 +48,20 @@ class BaseTest(unittest.TestCase):
 
     # setUp runs before each test case
     def setUp(self):
-        self.desired_capabilities['name'] = self.id()
-
-        if BaseTest.tunnel_id:
-            self.desired_capabilities['tunnel-identifier'] = BaseTest.tunnel_id
-        if BaseTest.build_tag:
-            self.desired_capabilities['build'] = BaseTest.build_tag
-
-        self.driver = webdriver.Remote(
-                command_executor="http://%s:%s@%s:%s/wd/hub" %
-                                 (BaseTest.username,
-                                  BaseTest.access_key,
-                                  BaseTest.selenium_host,
-                                  BaseTest.selenium_port),
-                desired_capabilities=self.desired_capabilities)
+        # self.desired_capabilities['name'] = self.id()
+        #
+        # if BaseTest.tunnel_id:
+        #     self.desired_capabilities['tunnel-identifier'] = BaseTest.tunnel_id
+        # if BaseTest.build_tag:
+        #     self.desired_capabilities['build'] = BaseTest.build_tag
+        #
+        # self.driver = webdriver.Remote(
+        #         command_executor="http://%s:%s@%s:%s/wd/hub" %
+        #                          (BaseTest.username,
+        #                           BaseTest.access_key,
+        #                           BaseTest.selenium_host,
+        #                           BaseTest.selenium_port),
+        #         desired_capabilities=self.desired_capabilities)
 
         # Open the website in the BaseTest, this can now be used in multiple tests, without
         # having to reload the page. Otherwise, individual tests can still reload the page
@@ -82,9 +82,9 @@ class BaseTest(unittest.TestCase):
     # tearDown runs after each test case
     def tearDown(self):
         self.driver.quit()
-        sauce_client = SauceClient(BaseTest.username, BaseTest.access_key)
-        status = (sys.exc_info() == (None, None, None))
-        sauce_client.jobs.update_job(self.driver.session_id, passed=status)
+        # sauce_client = SauceClient(BaseTest.username, BaseTest.access_key)
+        # status = (sys.exc_info() == (None, None, None))
+        # sauce_client.jobs.update_job(self.driver.session_id, passed=status)
         # test_name = "%s_%s" % (type(self).__name__, self.__name__)
         # with(open(test_name + '.testlog', 'w')) as outfile:
         #     outfile.write("SauceOnDemandSessionID=%s job-name=%s\n" % (self.driver.session_id, test_name))
